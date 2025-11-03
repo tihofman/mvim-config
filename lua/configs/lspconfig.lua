27c817782b86ba1d5eccb45c729c13a62a1fb591
Timm Hofmann <tihofmann@icloud.com>
2025-11-03 19:13:43 +0100

    my nvchad config

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,6 +1,7 @@
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { "html", "cssls" }
+local servers = { "html", "cssls", "jdtls", "kotlin_lsp", "angularls"}
 vim.lsp.enable(servers)
+-- require('lspconfig').jdtls.setup({})
 
 -- read :h vim.lsp.config for changing options of lsp servers 
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,28 +1,29 @@
 return {
   {
     "stevearc/conform.nvim",
-    -- event = 'BufWritePre', -- uncomment for format on save
+     event = 'BufWritePre', -- uncomment for format on save
     opts = require "configs.conform",
   },
-
-  -- These are some examples, uncomment them if you want to see them work!
+  {
+    'nvim-java/nvim-java',
+    config = function ()
+      require('java').setup()
+    end
+  },
   {
     "neovim/nvim-lspconfig",
     config = function()
       require "configs.lspconfig"
     end,
   },
-
-  -- test new blink
-  -- { import = "nvchad.blink.lazyspec" },
-
-  -- {
-  -- 	"nvim-treesitter/nvim-treesitter",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"vim", "lua", "vimdoc",
-  --      "html", "css"
-  -- 		},
-  -- 	},
-  -- },
+  { import = "nvchad.blink.lazyspec" },
+  {
+  	"nvim-treesitter/nvim-treesitter",
+  	opts = {
+  		ensure_installed = {
+  			"vim", "lua", "vimdoc",
+       "html", "css", "java"
+  		},
+  	},
+  },
 }
